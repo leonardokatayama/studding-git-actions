d223134c3ef69319d35d99405e2c98b9f44bc648
Leonardo Yoshitero Katayama <leonardo@akata.com.br>
2023-06-26 10:51:56 -0300

    yarn install

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.send({ ok: 'Hello World'})
+})
+
+app.listen('3000', () => {
+  console.log(`Server listening on port 3000 at http://localhost:3000`)
+})
